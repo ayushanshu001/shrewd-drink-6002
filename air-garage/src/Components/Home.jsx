0392b33aa5772b94d52dd1e1b29e5aba90eadb1c
Ayush Anshu <ayushanshu56@gmail.com>
2022-07-21 07:17:46 +0530

    day2-commit

--- air-garage/src/Components/Home.jsx
+++ air-garage/src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from './Footer'
 
 const Home = () => {
   return (
@@ -41,6 +42,61 @@ We operate hundreds of parking lots and garages across the
         <h4 style={{fontWeight:"lighter"}}>Our proprietary technology enables us to offer
              lower operating costs to our partners meaning 
              they take home up to 50% more every <br/>month than when they worked with our competition.</h4>
+
+
+    </div>
+    <div style={{display:"flex"}}>
+        <img style={{height:"29rem", width:"55rem", paddingLeft:"18.5rem"}} src="https://assets.website-files.com/5d55f1425cb6b7a18aa77528/619bf3a972f16c08c2a24729_stack-ag-p-1600.png"/>
+
+    </div>
+    <div style={{paddingTop:"5rem"}}>
+        <h1 style={{paddingRight:"38rem"}}>Our value proposition</h1>
+        <h4 style={{paddingRight:"12rem",fontWeight:"lighter"}}>We've built the operating system for parking management, infusing technology into every facet of the business.</h4>
+    </div>
+    <div style={{display:"flex"}}>
+        <div>
+            <img style={{height:"4rem", width:"4rem", paddingLeft:"9rem"}}src="https://assets.website-files.com/5d55f1425cb6b7a18aa77528/5f1a1f28039ee32a88bdfae6_cover.png"/>
+            <h1 style={{fontSize:"large", paddingLeft:"17rem"}}>Best-in-class Economics</h1>
+            <h4 style={{fontWeight:"lighter",textAlign:"left", paddingLeft:"20.5rem",fontSize:"medium"}}>Increase Net Operating Income <br/> your property by up to 50% vs other<br/> parking operators.</h4>
+        </div>
+        <div style={{textAlign:"left"}}>
+        <img style={{height:"4rem", width:"4rem", paddingLeft:"2rem"}}src="https://assets.website-files.com/5d55f1425cb6b7a18aa77528/5f1a1f2937a53c7a2bfb65b7_slider.png"/>
+        <h1 style={{fontSize:"large",paddingLeft:"2rem"}}>You're in Control</h1>
+        <h4 style={{fontWeight:"lighter",textAlign:"left",paddingLeft:"2rem"}}>Need to reserve spaces for<br/> customers or validate employee<br/> vehicles? Our system was built with <br/>multi-use cases in mind.</h4>
+        </div>
+        <div>
+        <img style={{height:"4rem", width:"4rem", paddingLeft:"2rem"}}src="https://assets.website-files.com/5d55f1425cb6b7a18aa77528/5f1a1f2957e7c929611df3bb_secure.png"/>
+        <h1 style={{fontSize:"large",paddingLeft:"2rem"}}>Always On Enforcement</h1>
+        <h4 style={{fontWeight:"lighter",textAlign:"left",paddingLeft:"2rem"}}>AirGarage's proprietary network of<br/> enforcement officers scans day and <br/>night so you don't have to stress<br/> about violators.</h4>
+        </div>
+    </div>
+    <div style={{height:"10rem", width:"50rem", backgroundColor:"#F7F7F7",marginLeft:"20rem", borderRadius:"0.5rem"}}>
+        <h1 style={{paddingTop:"2rem"}}>Get a better deal today</h1>
+        <h4 style={{fontWeight:"lighter"}}>Send us your parking management contract and get a counter in 24 hours</h4>
+
+    </div>
+    <div style={{display:"flex",paddingTop:"5rem"}}>
+        <div style={{flexDirection:"column"}}>
+        <img style={{paddingLeft:"16rem",height:"20rem", width:"25rem",borderRadius:"0.5rem"}}src="https://assets.website-files.com/5d55f1425cb6b7a18aa77528/60ad7c7bc5cb09481db5a7b3_Image%20from%20iOS%20(4)-p-500.jpeg"/>
+        <h1 style={{paddingLeft:"15rem"}}>Increase Parking Revenue</h1>
+        <h4 style={{textAlign:"left",marginLeft:"19rem",fontWeight:"lighter",fontSize:"medium",paddingRight:"2rem"}}>Other parking operators nickel and dime owners by passing<br/> through every cost of their operation from uniforms to phone.<br/> AirGarage operates on a revenue share model so that we only<br/> make money if you do.</h4>
+        
+
+        </div>
+        <div  style={{flexDirection:"column"}}>
+        <img style={{height:"20rem", width:"25rem",paddingRight:"22rem"}} src="https://assets.website-files.com/5d55f1425cb6b7a18aa77528/60ad7c7bb4dc18e48c224f8e_Image%20from%20iOS%20(5)-p-500.jpeg"/>
+        <h1 style={{paddingRight:"22.5rem"}}>Multifaceted Enforcement</h1>
+        <h4 style={{textAlign:"left",fontWeight:"lighter",fontSize:"medium"}}>The combined power of AirGarage's network of enforcement<br/> officers and our Automatic License Plate Recognition cameras<br/> means no illegally parked vehicle will slip through the cracks<br/> unnoticed.</h4>
+
+        </div>
+    </div>
+    <div style={{height:"10rem", width:"50rem", backgroundColor:"#F7F7F7",marginLeft:"20rem", borderRadius:"0.5rem"}}>
+        <h1 style={{paddingTop:"2rem"}}>Learn how we work for you</h1>
+        <h4 style={{fontWeight:"lighter"}}>Talk to sales today</h4>
+
+    </div>
+    <div style={{paddingTop:"5rem"}}>
+        <Footer/>
     </div>
     </>
   )
--- air-garage/src/Components/Navbar.jsx
+++ air-garage/src/Components/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = () => {
             <Link to="/"><img style={{height:"2rem", width:"9rem",paddingTop:"1rem"}}src="https://assets.website-files.com/5d55f1425cb6b7a18aa77528/5d55f8a49a649a59aab64b1c_airgarage-logo.png"/></Link>
         </div>
      
-     <div style={{display:"flex", gap:"2rem", paddingRight:"15rem"}}>
-     <Link to="/parking"><h5>Find parking</h5></Link>
-     <Link to="/login"><h5>Log in</h5></Link>
-     <Link to="/signup"><h5>Signup</h5></Link>
-     <Link to="/talk"><h5>Talk to Sales</h5></Link>
+     <div style={{display:"flex", gap:"2rem", paddingRight:"15rem", textDecoration:"none"}}>
+     <Link style={{textDecoration:"none",color:"black"}} to="/parking"><h5>Find parking</h5></Link>
+     <Link style={{textDecoration:"none",color:"black"}} to="/login"><h5>Log in</h5></Link>
+     <Link style={{textDecoration:"none",color:"black"}} to="/signup"><h5>Signup</h5></Link>
+     <Link style={{textDecoration:"none",color:"black"}} to="/talk"><h5>Talk to Sales</h5></Link>
      </div>
     </div>
   )
